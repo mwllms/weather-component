66f9e2abbba0f5d5013f229f3ad0947cf55cb091
Mark Willems <mrkwillems@gmail.com>
2021-10-27 20:13:34 +0200

    Tailwind

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { defineCustomElement } from 'vue'
 import App from './App.ce.vue'
+import './app.css'
 
 const weatherComponent = defineCustomElement(App)
 customElements.define('weather-app', weatherComponent)
